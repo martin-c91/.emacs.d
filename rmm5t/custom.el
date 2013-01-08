7a2822774e894ae9b465e0d4779ca659c22c229d
Martin C <narutocampus@gmail.com>
2013-01-08 02:47:27 -0800

    Fix magit so it detech .emacs.d repo

--- rmm5t/custom.el
+++ rmm5t/custom.el
@@ -3,8 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
-)
+ '(backup-by-copying t)
+ '(backup-directory-alist (quote (("" . "~/.emacs.d/backups"))))
+ '(kept-new-versions 10)
+ '(kept-old-versions 20)
+ '(magit-repo-dirs (quote ("~/.emacs.d")))
+ '(version-control t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
