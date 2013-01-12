e4e434ea61de088904a8e5f7704f4b144f99a64d
Martin C <narutocampus@gmail.com>
2013-01-12 01:37:14 -0800

    fixed auto comple initialzation

--- rmm5t/auto-complete.el
+++ rmm5t/auto-complete.el
@@ -1,4 +1,5 @@
 ;; Need this to load after vendor() it.
-(vendor 'auto-complete-config 'ac-config-default)
+(require 'auto-complete-config)
+(ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/ac-dict")
 (vendor 'popup)
