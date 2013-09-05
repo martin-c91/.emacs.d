db8eee72b71a7ecab6af753fae2f9099d7d51da6
Martin C <narutocampus@gmail.com>
2013-09-05 07:02:07 -0700

    Disabled ido search

--- rmm5t/ido.el
+++ rmm5t/ido.el
@@ -3,4 +3,4 @@
 (ido-mode t)
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory names
-
+(setq ido-auto-merge-work-directories-length -1) ; disable auto search because it's useless
