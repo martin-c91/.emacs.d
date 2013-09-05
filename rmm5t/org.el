55fb0af4b0525e42bf1d7d3d0ff81428647b4f60
Martin C <narutocampus@gmail.com>
2013-09-05 07:02:07 -0700

    Changed .org

--- rmm5t/org.el
+++ rmm5t/org.el
@@ -4,7 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+;; (setq org-log-done t)
 
 ;; (setq org-agenda-files (list "~/org/home.org"
 ;;                              "~/org/inbox.org"
