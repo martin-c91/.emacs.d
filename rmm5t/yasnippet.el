e834c9386cb7fdb631f098ef1b1f124828e67dcd
Martin Chea <martinchea@gmail.com>
2014-06-01 04:23:15 -0700

    fix yasnippet rmm5t file

--- rmm5t/yasnippet.el
+++ rmm5t/yasnippet.el
@@ -5,7 +5,7 @@
 (yas-global-mode 1)
 
 (yas-load-directory "~/.emacs.d/vendor/yasnippet/snippets")
-(yas-load-directory "~/.emacs.d/vendor/yasnippets-shoulda")
+;;(yas-load-directory "~/.emacs.d/vendor/yasnippets-shoulda")
 
 ;; (setq yas-prompt-functions '(yas-ido-prompt
 ;;                             yas-dropdown-prompt
